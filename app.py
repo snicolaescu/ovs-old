e15f969f7a697e023c6a88d219408b3e238a65b7
snicolaescu <sebastian.nicolaescu@gmail.com>
2014-12-18 14:33:23 -0500

    flask

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-print "hello world"
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def getRoot():
+    return "Hello World from getRoot"
+
+
+@app.route('/<name>', methods=['GET'])
+def getsub1(name):
+    return "Hello World from " + name  + " !" 
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=80, debug=True)
+    
